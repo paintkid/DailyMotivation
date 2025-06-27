a6f3f8735bcb91cd90fd42f297483c6f4e1e28c2
Joel Kim <joelminchankim@gmail.com>
2025-06-27 18:14:08 -0400

    Initial commit: set up DailyMotivation project with .gitignore

---
+++ DailyMotivation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyMotivation
+//
+//  Created by Joel Kim on 6/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyMotivation/DailyMotivationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMotivationApp.swift
+//  DailyMotivation
+//
+//  Created by Joel Kim on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyMotivationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
